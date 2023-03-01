710780871096d7f785ce18e87b64d513d8fef40d
Minor Gordon <minorg@server.fake>
2023-03-01 14:37:25 -0500

    enable prettier

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,7 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          // {to: '/blog', label: 'Blog', position: 'left'},
         ],
       },
       footer: {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,94 +1,61 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 type FeatureItem = {
-    title: string;
-    // Svg: React.ComponentType<React.ComponentProps<'svg'>>;
-    description: JSX.Element;
+  title: string;
+  // Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
-    {
-        title: 'Easy to Use',
-        // Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-        description: (
-            <>
-                Docusaurus was designed from the ground up to be easily installed and
-                used to get your website up and running quickly.
-            </>
-        ),
-    },
-    {
-        title: 'Focus on What Matters',
-        // Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-        description: (
-            <>
-                Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-                ahead and move your docs into the <code>docs</code> directory.
-            </>
-        ),
-    },
-    {
-        title: 'Powered by React',
-        // Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-        description: (
-            <>
-                Extend or customize your website layout by reusing React. Docusaurus can
-                be extended while reusing the same header and footer.
-            </>
-        ),
-    },
+  {
+    title: "Easy to Use",
+    // Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        Docusaurus was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.
+      </>
+    ),
+  },
+  {
+    title: "Focus on What Matters",
+    // Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+        ahead and move your docs into the <code>docs</code> directory.
+      </>
+    ),
+  },
+  {
+    title: "Powered by React",
+    // Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer.
+      </>
+    ),
+  },
 ];
 
 function Feature({title, description}: FeatureItem) {
-    return (
-        <div className={clsx('col col--4')}>
-            {/*<div className="text--center">*/}
-            {/*    <Svg className={styles.featureSvg} role="img" />*/}
-            {/*</div>*/}
-            <div className="text--center padding-horiz--md">
-                <h3>{title}</h3>
-                <p>{description}</p>
-            </div>
-        </div>
-    );
-}
-
-function HomepageFeatures(): JSX.Element {
-    return (
-        <section className={styles.features}>
-            <div className="container">
-                <div className="row">
-                    {FeatureList.map((props, idx) => (
-                        <Feature key={idx} {...props} />
-                    ))}
-                </div>
-            </div>
-        </section>
-    );
-}
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Get started ⏱️
-          </Link>
-        </div>
+    <div className={clsx("col col--4")}>
+      {/*<div className="text--center">*/}
+      {/*    <Svg className={styles.featureSvg} role="img" />*/}
+      {/*</div>*/}
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
       </div>
-    </header>
+    </div>
   );
 }
 
@@ -97,10 +64,32 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      description="Description will go into a meta tag in <head />"
+    >
+      <header className={clsx("hero hero--primary", styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/intro"
+            >
+              Get started ⏱️
+            </Link>
+          </div>
+        </div>
+      </header>
       <main>
-        <HomepageFeatures />
+        <section className={styles.features}>
+          <div className="container">
+            <div className="row">
+              {FeatureList.map((props, idx) => (
+                <Feature key={idx} {...props} />
+              ))}
+            </div>
+          </div>
+        </section>
       </main>
     </Layout>
   );
