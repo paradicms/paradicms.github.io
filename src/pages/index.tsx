b11c05b0c6ff07117ecc438cb1f1ed447552b6a1
Minor Gordon <minorg@server.fake>
2023-03-07 10:16:48 -0500

    remove hero banner logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -108,7 +108,6 @@ export default function Home(): JSX.Element {
     <Layout>
       <header className={clsx("hero hero--primary", styles.heroBanner)}>
         <div className="container">
-          <img className={styles.heroLogo} src="/img/logo.svg" />
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
