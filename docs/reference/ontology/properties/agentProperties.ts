ae7c9b08b6cde5e56a1f050c9d7c25ef8a45b59b
Minor Gordon <minorg@server.fake>
2023-04-17 09:56:46 -0400

    agentProperties and eventProperties

---
+++ docs/reference/ontology/properties/agentProperties.ts
@@ -0,0 +1,38 @@
+import PropertiesTableProperty from "@site/src/components/PropertiesTableProperty";
+
+const agentProperties: readonly PropertiesTableProperty[] = [
+  {
+    term: "name",
+    iri: "foaf:name",
+    description: "Human-readable name",
+    cardinality: "1",
+    valueType: "string",
+    exampleValues: "Alan Turing",
+  },
+  {
+    term: "page",
+    iri: "foaf:page",
+    description: "Website of the person",
+    cardinality: "0..n",
+    valueType: "string",
+    exampleValues: "https://mysite.com",
+  },
+  {
+    term: "relation",
+    iri: "dcterms:relation",
+    description: "Related IRI e.g., a Wikidata concept IRI",
+    cardinality: "0..n",
+    valueType: "IRI",
+    exampleValues: "http://www.wikidata.org/entity/Q7251",
+  },
+  {
+    term: "sortName",
+    iri: "contact:sortName",
+    description:
+      "Name to use in sorting the person in a list of names; if not specified, use name",
+    cardinality: "0..1",
+    valueType: "string",
+    exampleValues: "Alan Turing",
+  },
+];
+export default agentProperties;
--- docs/reference/ontology/properties/organizationProperties.ts
+++ docs/reference/ontology/properties/organizationProperties.ts
@@ -1,29 +1,5 @@
 import PropertiesTableProperty from "@site/src/components/PropertiesTableProperty";
+import agentProperties from "@site/docs/reference/ontology/properties/agentProperties";
 
-const organizationProperties: readonly PropertiesTableProperty[] = [
-  {
-    term: "name",
-    iri: "foaf:name",
-    description: "Human-readable name",
-    cardinality: "1",
-    valueType: "string",
-    exampleValues: "My company",
-  },
-  {
-    term: "page",
-    iri: "foaf:page",
-    description: "Website of the organization",
-    cardinality: "0..n",
-    valueType: "string",
-    exampleValues: "https://mycompany.com",
-  },
-  {
-    term: "relation",
-    iri: "dcterms:relation",
-    description: "Related IRI e.g., a Wikidata concept IRI",
-    cardinality: "0..n",
-    valueType: "IRI",
-    exampleValues: "http://www.wikidata.org/entity/Q49211",
-  },
-];
+const organizationProperties: readonly PropertiesTableProperty[] = agentProperties.concat();
 export default organizationProperties;
