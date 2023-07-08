8a13e75c17d40cb96732116bd3cbd8dd12f7bb3b
Minor Gordon <minorg@server.fake>
2023-07-08 11:08:54 -0400

    schemaTextObjectProperties

---
+++ docs/reference/ontology/properties/schemaTextObjectProperties.ts
@@ -0,0 +1,17 @@
+import PropertiesTableProperty from "@site/src/components/PropertiesTableProperty";
+import {schemaThingProperties} from "@site/docs/reference/ontology/properties/schemaThingProperties";
+
+const className = "SchemaTextObject";
+
+const schemaTextObjectProperties: readonly PropertiesTableProperty[] = [
+  ...schemaThingProperties({className, nameRequired: false}),
+  {
+    term: "text",
+    iri: "schema:text",
+    description: "Text value",
+    cardinality: "1",
+    valueType: "string",
+    exampleValues: '"Text with metadata"',
+  },
+];
+export default schemaTextObjectProperties;
